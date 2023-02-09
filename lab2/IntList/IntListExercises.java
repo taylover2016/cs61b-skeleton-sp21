a9e20af80902d136c97e80f55665a04d991aa025
taylover2016 <taylover2016@yahoo.com>
2023-02-09 13:26:57 +0800

    Lab 2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -54,8 +54,8 @@ public class IntListExercises {
         while (x > 10) {
             x = x / 10;
         }
-        int firstDigit = x % 10;
-        return firstDigit == lastDigit;
+        //int firstDigit = x % 10;
+        return x == lastDigit;
     }
 
     /**
